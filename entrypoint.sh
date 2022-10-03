eb57a7c04b338163c5478780ba3aee351bf2b32e
waiyanwh <waiyan@opn.ooo>
2022-10-03 14:46:24 +0700

    output test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/sh -l
 
+# Get arguments from action input
 VAULT_URL=$1
 PROJECT_NAME=$2
 ENVIRONMENT=$3
 SECRETS=$4
 
-echo "VAULT url: $VAULT_URL"
-echo "Project name: $PROJECT_NAME"
-echo "Environment name: $ENVIRONMENT"
-
 for i in ${SECRETS}
 do
- echo "Secrets name: $i"
+# curl -s --header "X-Vault-Token: ${VAULT_TOKEN}" --request GET $VAULT_URL/v1/${PROJECT_NAME}/data/${ENVIRONMENT} | jq -r ".data.data${SECRET_NAME}"
+ echo "::set-output name=$i::$i"
 done
 
