270ad3eda143dea624ff295a8fe46a2a61f0c882
waiyanwh <waiyan@opn.ooo>
2022-10-03 14:50:22 +0700

    output test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ SECRETS=$4
 for i in ${SECRETS}
 do
 # curl -s --header "X-Vault-Token: ${VAULT_TOKEN}" --request GET $VAULT_URL/v1/${PROJECT_NAME}/data/${ENVIRONMENT} | jq -r ".data.data${SECRET_NAME}"
- echo "::set-output name=$i::$i"
+ echo "::set-output name=time::$i"
 done
 
