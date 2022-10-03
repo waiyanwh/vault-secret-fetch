10ce40c58dfe0d2f4955fe3a7c98c593565a7911
waiyanwh <waiyan@opn.ooo>
2022-10-03 14:20:48 +0700

    Initial for vault

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+VAULT_URL=$1
+PROJECT_NAME=$2
+ENVIRONMENT=$3
+SECRETS=$4
+
+echo "VAULT url: $VAULT_URL"
+echo "Project name: $PROJECT_NAME"
+echo "Environment name: $ENVIRONMENT"
+
+for i in SECRETS
+do
+ echo "Secrets name: $i"
+done
+
