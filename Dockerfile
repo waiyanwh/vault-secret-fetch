c29b44a926855f2fa60d0012f3850343289c7ebc
waiyanwh <waiyan@opn.ooo>
2022-10-05 13:35:59 +0700

    add: stable action

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM alpine:3.10
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
+RUN apk add --no-cache --update curl jq
 COPY entrypoint.sh /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,9 @@ VAULT_URL=$1
 PROJECT_NAME=$2
 ENVIRONMENT=$3
 SECRETS=$4
-time=$(date)
 
-# for i in ${SECRETS}
-# do
-# # curl -s --header "X-Vault-Token: ${VAULT_TOKEN}" --request GET $VAULT_URL/v1/${PROJECT_NAME}/data/${ENVIRONMENT} | jq -r ".data.data${SECRET_NAME}"
-#  echo "::set-output name=secrets::$time"
-# done
-
-# echo "::set-output name=blah::$time"
-echo "::set-output name=action_fruit::strawberry"
+for i in ${SECRETS}
+do
+ result=$(curl -s --header "X-Vault-Token: ${VAULT_TOKEN}" --request GET $VAULT_URL/v1/${PROJECT_NAME}/data/${ENVIRONMENT} | jq -r ".data.data.$i")
+ echo "::set-output name=$i::$result"
+done
